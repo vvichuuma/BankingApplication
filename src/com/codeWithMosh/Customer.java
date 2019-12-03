12bc585a0082a8492db973f912644fbbf23301cb
vishnu <vvichugit@gmail.com>
2019-12-03 13:07:37 -0600

    added the Customer class

--- src/com/codeWithMosh/Customer.java
+++ src/com/codeWithMosh/Customer.java
@@ -1,4 +1,35 @@
 package com.codeWithMosh;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public  Customer(String name, double amount){
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+        addTransactions(amount);
+    }
+
+    //addTransactions:
+    public void addTransactions(double amount){
+        this.transactions.add(amount);
+    }
+
+    //returns the String name:
+    public String getName(String name){
+        return this.name ;
+    }
+
+    //returns the array list of transactions:
+    public ArrayList<Double> getTransactions(){
+        return this.transactions;
+    }
+
+    public void testCode(){
+        System.out.println("Hey name is " + this.name + " and transactions " + this.transactions);
+    }
+
 }
--- src/com/codeWithMosh/Main.java
+++ src/com/codeWithMosh/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Code works!!");
+        Customer c1 = new Customer("Vish", 2.14);
+        c1.addTransactions(34.21);
+        c1.testCode();
 
     }
 }
